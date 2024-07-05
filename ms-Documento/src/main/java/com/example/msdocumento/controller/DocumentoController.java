d399b18d722b2977765de1bc3b90e58aa47ef7e3
LuisAlbertoQ <103071836+LuisAlbertoQ@users.noreply.github.com>
2024-07-04 21:59:08 -0500

    documentos

--- ms-Documento/src/main/java/com/example/msdocumento/controller/DocumentoController.java
+++ ms-Documento/src/main/java/com/example/msdocumento/controller/DocumentoController.java
@@ -1,7 +1,9 @@
 package com.example.msdocumento.controller;
 
 import com.example.msdocumento.entity.Documento;
+import com.example.msdocumento.entity.UserDto;
 import com.example.msdocumento.service.DocumentoService;
+import com.example.msdocumento.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,9 +23,15 @@ public class DocumentoController {
     @Autowired
     public DocumentoService documentoService;
 
+    @Autowired
+    public UserService userService;
+
     @PostMapping("/subir")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam(value = "replace", defaultValue = "false") boolean replace) {
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
+                                             @RequestParam(value = "replace", defaultValue = "false") boolean replace,
+                                             @RequestHeader("Authorization") String jwt) {
         try {
+            UserDto user = userService.getUserProfile(jwt);
             documentoService.saveFile(file, replace);
             return ResponseEntity.status(HttpStatus.OK).body("Archivo Subido Exitosamente! :D");
         } catch (IOException e) {
@@ -34,10 +42,12 @@ public class DocumentoController {
     }
 
     @GetMapping("/descargar/{idDocumento}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable Long idDocumento) {
-        Documento documento = documentoService.tomarDocumento(idDocumento);
-        if (documento != null) {
-            try {
+    public ResponseEntity<byte[]> downloadFile(@PathVariable Long idDocumento,
+                                               @RequestHeader("Authorization") String jwt) {
+        try {
+            UserDto user = userService.getUserProfile(jwt);
+            Documento documento = documentoService.tomarDocumento(idDocumento);
+            if (documento != null) {
                 File file = new File(documento.getRutaArchivo());
                 FileInputStream fileInputStream = new FileInputStream(file);
                 byte[] data = fileInputStream.readAllBytes();
@@ -47,17 +57,22 @@ public class DocumentoController {
                         .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + documento.getNombreDocumento() + "\"")
                         .header(HttpHeaders.CONTENT_TYPE, documento.getTipoArchivo())
                         .body(data);
-            } catch (IOException e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
     @GetMapping("/listar")
-    public ResponseEntity<List<Documento>> listDocuments() {
-        List<Documento> documentos = documentoService.listardocumento();
-        return ResponseEntity.ok().body(documentos);
+    public ResponseEntity<List<Documento>> listDocuments(@RequestHeader("Authorization") String jwt) {
+        try {
+            UserDto user = userService.getUserProfile(jwt);
+            List<Documento> documentos = documentoService.listardocumento();
+            return ResponseEntity.ok().body(documentos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
